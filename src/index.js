bb1ed91feccd8813c93609f992cd538b252cf577
giogonzal21 <giovannygonzalez806@gmail.com>
2024-04-11 19:10:16 -0500

    Node 2

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const server = http.createServer(function(req, res) {
 });
 server.listen(5000);*/
 
-const express = require('express')
+/*const express = require('express')
 const app = express()
 const port = 3000
 
@@ -19,4 +19,26 @@ app.get ('/', (req, res) => {
 
 app.listen (port, () => {
     console.log('La aplicacion se esta ejecutando por el puerto ' + port)
-})
+})*/
+const parser = require("body-parser");
+const express = require('express');
+const app = express();
+const port = 3000;
+const animalRoutes = require("./routes/animalRoutes");
+const mongoose = require("mongoose");
+require('dotenv').config();
+app.use(parser.urlencoded({ extended: false })); //permite leer los datos que vienen en la petición
+app.use(parser.json()); // transforma los datos a formato JSON
+//Gestión de las rutas usando el middleware
+app.use("/api", animalRoutes);
+app.use(express.json());
+//Conexión a la base de datos
+mongoose
+    .connect(process.env.MONGODB_URI)
+    .then(() => console.log("Conexión exitosa"))
+    .catch((error) => console.log(error));
+//Conexión al puerto
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+});
+
